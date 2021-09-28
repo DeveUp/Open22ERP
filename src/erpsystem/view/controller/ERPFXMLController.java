16e53af1e5843a764c63cc7f4bb08d2105fee81d
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-09-28 15:42:58 -0500

    Controller Main #12

--- src/erpsystem/view/controller/ERPFXMLController.java
+++ src/erpsystem/view/controller/ERPFXMLController.java
@@ -3,9 +3,16 @@ package erpsystem.view.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class ERPFXMLController implements Initializable {
+	
+	@FXML
+	private JFXButton buttonPerson;
 	
 	public ERPFXMLController() {
 	}
@@ -14,5 +21,9 @@ public class ERPFXMLController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		
 	}
-
+	
+	@FXML
+	public void onButtonPerson(ActionEvent event) {
+		System.out.println("SSSS");
+	}
 }
